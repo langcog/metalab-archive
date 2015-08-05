45f1896bc8270c2c61c32863885777f86cd02c24
Mika Braginsky <mika.br@gmail.com>
2015-08-05 20:47:24 +0000

    dataset caching in scripts dir, copy of crontab #16

--- scripts/cache_datasets.sh
+++ scripts/cache_datasets.sh
@@ -2,9 +2,10 @@
 # Runs the cache_datasets R script, writes the results to a log file, and sends each line of the log file
 # to LangCog's Slack under #metalab-log channel
 
+cd /home/ubuntu/metalab/
 LOGDATE=$(date +%Y-%m-%dT%H:%M:%S)
 LOGFILE=log/cache_datasets_$LOGDATE
-Rscript cache_datasets.R > $LOGFILE
+Rscript scripts/cache_datasets.R > $LOGFILE
 
 SLACKPOSTURL=https://hooks.slack.com/services/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
 
