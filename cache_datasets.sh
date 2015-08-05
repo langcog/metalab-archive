9b565080de69fd663aa355094f8db106c109d0e6
Mika Braginsky <mika.br@gmail.com>
2015-08-05 20:32:10 +0000

    bash script to run cache_datasets.R and write results to log file and slack #16

---
+++ cache_datasets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Runs the cache_datasets R script, writes the results to a log file, and sends each line of the log file
+# to LangCog's Slack under #metalab-log channel
+
+LOGDATE=$(date +%Y-%m-%dT%H:%M:%S)
+LOGFILE=log/cache_datasets_$LOGDATE
+Rscript cache_datasets.R > $LOGFILE
+
+SLACKPOSTURL=https://hooks.slack.com/services/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
+
+while read -r line
+do
+    ENTRY=$line
+    curl -X POST --data-urlencode 'payload={"channel": "#metalab-log", "username": "cachedatasetbot", "icon_emoji": ":dog:", "text": "'"$ENTRY"'"}' $SLACKPOSTURL
+done < $LOGFILE
