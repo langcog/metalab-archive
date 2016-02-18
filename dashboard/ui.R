9418aab420b3ae974393fdd73b28d141d76550b7
Mika Braginsky <mikabr@stanford.edu>
2016-02-17 18:15:24 -0800

    ui tabs separated out, overview tab creates dataset boxes from loop, documentation tab render overview doc

--- dashboard/ui.R
+++ dashboard/ui.R
@@ -12,121 +12,77 @@ sidebar <- dashboardSidebar(
              icon = icon("signal", lib = "glyphicon")),
     menuItem("Reports", tabName = "reports",
              icon = icon("folder-open", lib = "glyphicon")),
-    menuItem("Power Analysis", tabName = "power_analysis",
+    menuItem("Power Analysis", tabName = "power",
              icon = icon("flash", lib = "glyphicon"))
   )
 )
 
-body <- dashboardBody(
-  # replace with includeCSS
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+tab_overview <- tabItem(
+  tabName = "overview",
+  wellPanel(
+    div(class = "text-center",
+        fluidRow(column(width = 12, h1("MetaLab"),
+                        p(class = "lead", "A tool for power analysis and experimental",
+                          br(), "planning in language acquisition research")
+        )
+        )
+    )
   ),
-  tabItems(
-    tabItem(tabName = "overview",
-            wellPanel(
-              div(class = "text-center",
-                  fluidRow(column(width = 12, h1("MetaLab"),
-                                  p(class = "lead", "A tool for power analysis and experimental",
-                                    br(), "planning in language acquisition research")
-                  )
-                  )
-              )
-            ),
-            br(),
-            h3("Meta-analyses currently in MetaLab:"),
-            fluidRow(
-#              column(width = 12,
-                     box(width = 6,
-                       fluidRow(
-                         column(width = 3,
-                                img(src = "images/dinosaur.png", width = 100)),
-                         column(width = 9,
-                                h3("Phonemic discrimination"))
-                       )
-                     ),
-                     box(width = 6,
-                         fluidRow(
-                           column(width = 3,
-                                  img(src = "images/dinosaur.png", width = 100)),
-                           column(width = 9,
-                                  h3("Word segmentation"))
-                         )
-                     ),
-                     box(width = 6,
-                         fluidRow(
-                           column(width = 3,
-                                  img(src = "images/dinosaur.png", width = 100)),
-                           column(width = 9,
-                                  h3("Infant directed speech preference"))
-                         )
-                     ),
-                     box(width = 6,
-                         fluidRow(
-                           column(width = 3,
-                                  img(src = "images/dinosaur.png", width = 100)),
-                           column(width = 9,
-                                  h3("Pointing"))
-                         )
-                     ),
-                     box(width = 6,
-                         fluidRow(
-                           column(width = 3,
-                                  img(src = "images/zot.png", width = 100)),
-                           column(width = 9,
-                                  h3("Mutual exclusivity"))
-                         )
-                     ),
-                     box(width = 6,
-                       fluidRow(
-                         column(width = 3,
-                                img(src = "images/dinosaur.png", width = 100)),
-                         column(width = 9,
-                                h3("Label advantage in concept learning"))
-                       )
-                     )
-#              )
-#               column(width = 12,
-#                      box(
-#                        fluidRow(
-#                          column(width = 3,
-#                                 img(src = "images/dinosaur.png", width = 70, height = 100)),
-#                          column(width = 9,
-#                                 h2("Mutual exclusivity"))
-#                        )
-#                      )
-#               )
-            )
-    #includeRmd("../overview.Rmd", list('datasets' = datasets))
-  ),
-  tabItem(tabName = "documentation"),
-  tabItem(tabName = "visualizations",
+  br(),
+  h3("Meta-analyses currently in MetaLab:"),
+  fluidRow(
+    map(1:nrow(datasets), function(i) {
+      dataset <- datasets[i,]
+      box(width = 6,
           fluidRow(
-            column(width = 6,
-                   box(width = NULL, status = "danger",
-                       selectInput("dataset_name", label = "Dataset", choices = datasets$name),
-                       uiOutput("moderator_input")
-                       # checkboxGroupInput("moderators", label = "Moderators",
-                       #                    choices = list("Age" = "mean_age",
-                       #                                   "Exposure phase" = "exposure_phase",
-                       #                                   "Response mode" = "response_mode"),
-                       #                    inline = TRUE)
-                   ),
-                   box(width = NULL, status = "danger", plotOutput("scatter")),
-                   box(width = NULL, status = "danger", plotOutput("violin")),
-                   box(width = NULL, status = "danger", plotOutput("funnel"))
-            ),
-            column(width = 6,
-                   fluidRow(valueBoxOutput("studies_box"),
-                            valueBoxOutput("effect_size_box"),
-                            valueBoxOutput("effect_size_var_box")),
-                   fluidRow(box(width = NULL, status = "danger", plotOutput("forest")))
-            )
+            column(width = 3,
+                   img(src = dataset$src, width = 100)),
+            column(width = 9,
+                   h3(dataset$name))
           )
-  ),
-  tabItem(tabName = "reports"),
-  tabItem(tabName = "power_analysis")
+      )
+    })
+  )
 )
+
+tab_documentation <- tabItem(
+  tabName = "documentation",
+  includeRmd("../overview.Rmd", list("datasets" = datasets))
+)
+
+tab_visualizations <- tabItem(
+  tabName = "visualizations",
+  fluidRow(
+    column(width = 6,
+           box(width = NULL, status = "danger",
+               selectInput("dataset_name", label = "Dataset", choices = datasets$name),
+               uiOutput("moderator_input")
+           ),
+           box(width = NULL, status = "danger", plotOutput("scatter")),
+           box(width = NULL, status = "danger", plotOutput("violin")),
+           box(width = NULL, status = "danger", plotOutput("funnel"))
+    ),
+    column(width = 6,
+           fluidRow(valueBoxOutput("studies_box"),
+                    valueBoxOutput("effect_size_box"),
+                    valueBoxOutput("effect_size_var_box")),
+           fluidRow(box(width = NULL, status = "danger", plotOutput("forest")))
+    )
+  )
+)
+
+tab_reports <- tabItem(
+  tabName = "reports"
+)
+
+tab_power <- tabItem(
+  tabName = "power"
+)
+
+body <- dashboardBody(
+  includeCSS("www/custom.css"),
+  tabItems(tab_overview, tab_documentation, tab_visualizations,
+           tab_reports, tab_power)
 )
 
 dashboardPage(skin = "red", header, sidebar, body)
