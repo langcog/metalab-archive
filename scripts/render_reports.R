7b9f4ba8a518362e2a3a59967d40687a135c35b2
Mika Braginsky <mikabr@stanford.edu>
2016-05-09 16:19:19 -0700

    report rendering path fix #46

--- scripts/render_reports.R
+++ scripts/render_reports.R
@@ -1,3 +1,3 @@
-reports <- yaml::yaml.load_file("../metadata/reports.yaml")
-purrr::walk(reports, ~rmarkdown::render(sprintf("../reports/%s.Rmd", .x$file),
-                                        output_dir = "../reports/out"))
+reports <- yaml::yaml.load_file("metadata/reports.yaml")
+purrr::walk(reports, ~rmarkdown::render(sprintf("reports/%s.Rmd", .x$file),
+                                        output_dir = "reports/out"))
