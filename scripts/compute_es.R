7744335a57700207459ce9783b097a296ddd7a67
shotsuji <tsujish@gmail.com>
2017-04-07 11:15:57 -0400

    Change of within_one and within_two d_calc formulae
    As documented in corresponding issue.
    Also added descriptions

--- scripts/compute_es.R
+++ scripts/compute_es.R
@@ -1,3 +1,7 @@
+#This script calculates effect sizes for entries in metalab
+#metalab.stanford.edu
+#for questions please contact team members on metalab website
+
 library(dplyr)
 library(purrr)
 library(assertthat)
@@ -18,54 +22,63 @@ compute_es <- function(participant_design, x_1 = NA, x_2 = NA, x_dif = NA,
                        special_cases_measures = NA, contrast_sampa = NA) {
 
   assert_that(participant_design %in% c("between", "within_two", "within_one"))
-
+  
+  #we introduce variables calles d_calc and d_var_calc to distiguish them from the fields d and d_var, which are fields where effect sizes were already available from the source of the data
   d_calc <- NA
   d_var_calc <- NA
   es_method <- "missing"
 
+  #start of decision tree where effect sizes are calculated differently based on participant design
+  #depending on which data is available, effect sizes are calculated differently
   if (participant_design == "between") {
     es_method  <- "between"
+    #effect size calculation
     if (complete(x_1, x_2, SD_1, SD_2)) {
-      pooled_SD <- sqrt(((n_1 - 1) * SD_1 ^ 2 + (n_2 - 1) * SD_2 ^ 2) / (n_1 + n_2 - 2))
-      d_calc <- (x_1 - x_2) / pooled_SD
+      pooled_SD <- sqrt(((n_1 - 1) * SD_1 ^ 2 + (n_2 - 1) * SD_2 ^ 2) / (n_1 + n_2 - 2)) # Lipsey & Wilson, 3.14
+      d_calc <- (x_1 - x_2) / pooled_SD # Lipsey & Wilson (2001)
     } else if (complete(t)) {
-      d_calc <- t * sqrt((n_1 + n_2) / (n_1 * n_2))
+      d_calc <- t * sqrt((n_1 + n_2) / (n_1 * n_2)) # Lipsey & Wilson, (2001)
     } else if (complete(f)) {
-      d_calc <- sqrt(f * (n_1 + n_2) / (n_1 * n_2))
+      d_calc <- sqrt(f * (n_1 + n_2) / (n_1 * n_2)) # Lipsey & Wilson, (2001)
     }
     if (complete(n_1, n_2, d_calc)) {
+      #now that effect size are calculated, effect size variance is calculated
       d_var_calc <- ((n_1 + n_2) / (n_1 * n_2)) + (d_calc ^ 2 / (2 * (n_1 + n_2)))
     } else if (complete(r)) {
+      #if r instead of d is reported, transform for standardization
       d_calc <- 2 * r / sqrt(1 - r ^ 2)
       d_var_calc <- 4 * r_var / ((1 - r ^ 2) ^ 3)
     } else if (complete(d, d_var)) {
+      #if d and d_var were already reported, use those values
       d_calc <- d
       d_var_calc <- d_var
     }
 
   } else if (participant_design == "within_two") {
     if (is.na(corr)) {
+      #if correlation between two measures is not reported, use an imputed correlation value
       corr <- corr_imputed
     }
-    if (complete(x_1, x_2, SD_1, SD_2)) {
-      pooled_SD <- sqrt((SD_1 ^ 2 + SD_2 ^ 2) / 2)
-      d_calc <- (x_1 - x_2) / pooled_SD
+    #effect size calculation
+    if (complete(x_1, x_2, SD_1, SD_2)) { 
+      pooled_SD <- sqrt((SD_1 ^ 2 + SD_2 ^ 2) / 2) # Lipsey & Wilson (2001)
+      d_calc <- (x_1 - x_2) / pooled_SD # Lipsey & Wilson (2001)
       es_method  <- "group_means_two"
     } else if (complete(x_1, x_2, SD_dif)) {
-      within_SD <- SD_dif / sqrt(2 * (1 - corr))
-      d_calc <- (x_1 - x_2) / within_SD
+      within_SD <- SD_dif / sqrt(2 * (1 - corr)) # Lipsey & Wilson (2001); Morris & DeShon (2002)
+      d_calc <- (x_1 - x_2) / within_SD # Lipsey & Wilson (2001)
       es_method  <- "group_means_two"
     } else if (complete(x_dif, SD_1, SD_2)) {
-      pooled_SD <- sqrt((SD_1 ^ 2 + SD_2 ^ 2) / 2)
-      d_calc <- x_dif / pooled_SD
+      pooled_SD <- sqrt((SD_1 ^ 2 + SD_2 ^ 2) / 2) # Lipsey & Wilson (2001)
+      d_calc <- x_dif / pooled_SD # Lipsey & Wilson (2001)
       es_method  <- "subj_diff_two"
     } else if (complete(x_dif, SD_dif)) {
       wc <- sqrt(2 * (1 - corr))
-      d_calc <- (x_dif / SD_dif) * wc
+      d_calc <- (x_dif / SD_dif) * wc #Morris & DeShon (2002)
       es_method  <- "subj_diff_two"
     } else if (complete(t)) {
       wc <- sqrt(2 * (1 - corr))
-      d_calc <- (t / sqrt(n_1)) * wc
+      d_calc <- (t / sqrt(n_1)) * wc #Dunlap et al., 1996, p.171
       es_method  <- "t_two"
     } else if (complete(f)) {
       wc <- sqrt(2 * (1 - corr))
@@ -73,12 +86,16 @@ compute_es <- function(participant_design, x_1 = NA, x_2 = NA, x_dif = NA,
       es_method  <- "f_two"
     }
     if (complete(n_1, d_calc)) {
-      d_var_calc <- ((1 / n_1) + (d_calc ^ 2 / (2 * n_1))) * 2 * (1 - corr)
+      #now that effect size are calculated, effect size variance is calculated
+      #d_var_calc <- ((1 / n_1) + (d_calc ^ 2 / (2 * n_1))) * 2 * (1 - corr) #we used this until 4/7/17
+      d_var_calc <- (2 * (1 - corr)/ n_1) + (d_calc ^ 2 / (2 * n_1)) # Lipsey & Wilson (2001)
     } else  if (complete(r)) {
+      #if r instead of d is reported, transform for standardization
       d_calc <- 2 * r / sqrt(1 - r ^ 2)
       d_var_calc <- 4 * r_var / ((1 - r ^ 2) ^ 3)
       es_method  <- "r_two"
     } else if (complete(d, d_var)) {
+      #if d and d_var were already reported, use those values
       d_calc <- d
       d_var_calc <- d_var
       es_method  <- "d_two"
@@ -96,8 +113,8 @@ compute_es <- function(participant_design, x_1 = NA, x_2 = NA, x_dif = NA,
       es_method  <- "f_one"
     }
     if (complete(n_1, d_calc)) {
-      #d_var_calc <- (1 / n_1) + (d_calc ^ 2 / (2 * n_1))
-      d_var_calc <- (2/n_1) + (d_calc ^ 2 / (4 * n_1))
+      #d_var_calc <- (2/n_1) + (d_calc ^ 2 / (4 * n_1)) #we used this until 4/7/2017
+      d_var_calc <- (1 / n_1) + (d_calc ^ 2 / (2 * n_1)) #this models what is done in metafor package, escalc(measure="SMCR"() (Viechtbauer, 2010)
       
     } else if (complete(r, n_1)){ 
       # this deals with pointing and vocabulary 
@@ -115,19 +132,21 @@ compute_es <- function(participant_design, x_1 = NA, x_2 = NA, x_dif = NA,
       es_method  <- "r_one"
       
     } else if (complete(r)) {
+      #if r instead of d is reported, transform for standardization
       d_calc <- 2 * r / sqrt(1 - r ^ 2)
       d_var_calc <- 4 * r_var / ((1 - r ^ 2) ^ 3)
      
        es_method  <- "r_one"
       
     } else  if (complete(d, d_var)) {
+      #if d and d_var were already reported, use those values
       d_calc <- d
       d_var_calc <- d_var
       es_method  <- "d_one"
     }
   }
 
-  # SPECIAL CASES
+  # SPECIAL CASES for which an unusual subset of measures was reported
   if (!complete(d_calc, d_var_calc)) {
     es_method <- "special_case"
     
@@ -169,7 +188,8 @@ compute_es <- function(participant_design, x_1 = NA, x_2 = NA, x_dif = NA,
     if (complete(n_1, n_2, d_calc)) {
       d_var_calc <- ((n_1 + n_2) / (n_1 * n_2)) + (d_calc ^ 2 / (2 * (n_1 + n_2)))
     } else if (complete(n_1, d_calc)) {
-      d_var_calc <- (2/n_1) + (d_calc ^ 2 / (4 * n_1))
+      #d_var_calc <- (2/n_1) + (d_calc ^ 2 / (4 * n_1)) #we used this until 4/7/2017
+      d_var_calc <- (1/n_1) + (d_calc ^ 2 / (2 * n_1))
     }
   }
 
