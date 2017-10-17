15d48cd265d31d6beab7b426b176281333719fd8
Molly Lewis <mollyllewis@gmail.com>
2017-10-17 16:04:49 -0500

    paper edits for resubmission

--- write-ups/synthesis_paper/open_mind/agl_data_comparision.R
+++ write-ups/synthesis_paper/open_mind/agl_data_comparision.R
@@ -1,6 +1,15 @@
 source("../../../dashboard/global.R", chdir = TRUE)
 
-m <- filter(all_data,dataset == "Statistical sound category learning")
+#m <- filter(all_data,dataset == "Statistical sound category learning")
+
+m <- filter(all_data, short_name == "symbolism") %>%
+  select(study_ID, x_1, x_2, d_calc, d_var_calc, condition_type, infant_type)
+
+model = metafor::rma(d_calc, d_var_calc, data = m)
+data.frame(dataset = ma_data$short_name[1],
+           overall.d = model$b,
+           ci_lower = model$ci.lb,
+           ci_upper = model$ci.ub)
 
 m_clean <- m %>%
   select(study_ID, 
