849fb71f1d56f0d22a547aec51e630fc53536c5b
Molly Lewis <mollyllewis@gmail.com>
2017-10-24 12:21:50 -0500

    almost ready to submit

--- write-ups/synthesis_paper/paper_scripts/cache_paper_data.R
+++ write-ups/synthesis_paper/paper_scripts/cache_paper_data.R
@@ -2,8 +2,8 @@
 # because the data in the repository change frequently, and not all MAs in 
 # metalab are reported in the paper. This file reads in the data that has been
 # cached by the scripts/cache_datasets.R script, and caches a second version of 
-# the data to be used for the paper in ../paper_data/. To run this file, from main
-# dir: Rscript write-ups/synthesis_paper/paper_analysis_scripts/cache_paper_data.R
+# the data to be used for the paper in ../paper_data/. To run this script, from main
+# dir: Rscript write-ups/synthesis_paper/paper_scripts/cache_paper_data.R
 # Thus, when data has been updated in the spreadsheets, two scripts need to be 
 # run in order to include it in the paper (cache_datasets.R and cache_paper_data.R).
 
@@ -21,7 +21,8 @@ source("dashboard/global.R", chdir = TRUE)
 
 ########################  ALL_DATA ########################   
 # remove inappropriate conditions and rename MAs
-synthesis_paper_all_data <- filter(all_data, short_name %in% MAS) %>%
+synthesis_paper_all_data <- all_data %>%
+  filter(short_name %in% MAS) %>%
   filter(is.na(condition_type) | condition_type == "critical") %>% # remove control conditions from labadv
   filter(infant_type == "typical") %>%
   filter(coder != "Alvaro Iturralde") %>% # can update to new data later
@@ -61,6 +62,3 @@ synthesis_paper_datasets <- datasets %>%
 
 # write datasets to feather
 write_feather(synthesis_paper_datasets, "write-ups/synthesis_paper/paper_data/synthesis_paper_datasets.feather")
-
-
-
